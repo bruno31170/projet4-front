fe7ae068b737562a32f1d949220b85648ebd100a
bruno31170 <bru.boulet@gmail.com>
2022-04-09 14:30:26 +0200

    test parametrage heroku

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static("./dist/angular-10-client"));
+app.use(express.static(_dirname + "/dist/angular-10-client"));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-10-client/" })
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(_dirname + "/dist/angular-10-client/index.html"));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT);
